5679de4ba940bf119b4281b6d52ae9b22e08880c
Sagar Savaliya <savaliya.sagar97@gmail.com>
2025-05-24 01:45:02 +0530

    Updated the plugin version after the fixes
    Updated the plugin version after the fixes

--- infinite-uploads.php
+++ infinite-uploads.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Infinite Uploads
  * Description: Infinitely scalable cloud storage and delivery for your videos and uploads made easy! Upload directly to cloud storage and manage your files right from the WordPress Media Library.
- * Version: 2.0.8
+ * Version: 2.0.9
  * Author: Infinite Uploads
  * Author URI: https://infiniteuploads.com/?utm_source=iup_plugin&utm_medium=plugin&utm_campaign=iup_plugin&utm_content=meta
  * Text Domain: infinite-uploads
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 
 Requires at least: 5.3
 Tested up to: 6.6
-Stable tag: 2.0.8
+Stable tag: 2.0.9
 Requires PHP: 7.0
 Contributors: uglyrobot, jdailey
 Tags: cloud storage, media cloud, encoding, offload, video, streaming, s3, google cloud storage, spaces, storage, cdn, cloud sync
