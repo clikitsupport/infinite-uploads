8024a52d22e143bad55fe52890fe644121cf5ee8
ML <mladuclikitnow@gmail.com>
2026-02-09 19:29:56 +0530

    Fixed sidebar CSS issue

--- inc/assets/js/media-folders.js
+++ inc/assets/js/media-folders.js
@@ -105,11 +105,10 @@
 						// Folder tree (user folders only)
 						'<div id="iu-folders-tree"></div>' +
 					'</div>' +
-					'<div class="iu-sidebar-handle">' +
-						'<span class="iu-sidebar-toggle" title="Toggle folders">' +
-							'<svg viewBox="0 0 24 24" fill="currentColor"><path d="M10.5 17a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.42L13.1 12 9.92 8.69a1 1 0 0 1 0-1.41 1 1 0 0 1 1.42 0l3.86 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-.7.32z"></path></svg>' +
-						'</span>' +
-					'</div>' +
+					'<div class="iu-sidebar-handle"></div>' +
+					'<span class="iu-sidebar-toggle" title="Toggle folders">' +
+						'<svg viewBox="0 0 24 24" fill="currentColor"><path d="M10.5 17a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.42L13.1 12 9.92 8.69a1 1 0 0 1 0-1.41 1 1 0 0 1 1.42 0l3.86 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-.7.32z"></path></svg>' +
+					'</span>' +
 				'</div>';
 
 			if (this.isListMode) {
